9a4bd2322e852e594fbcb01db885b0e9beea2d6e
方觉 (Fang Jue) <fangjue23303@gmail.com>
2014-04-23 14:32:38 +0800

    Use Event page, activeTab permission and executeScript.

--- save_all_images/main.js
+++ save_all_images/main.js
@@ -1,17 +1,3 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-	if(message == 'download'){
-		var urls = get_images();
-		sendResponse(urls);
-	}
+[].map.call(document.getElementsByTagName('img'), function(img){
+  return img.src;
 });
-
-function get_images(){
-	var img = document.getElementsByTagName('img');
-	var urls = [];
-	for(var i=0,imgLen=img.length; i<imgLen; i++){
-		if(img[i].src){
-			urls.push(img[i].src);
-		}
-	}
-	return urls;
-}
